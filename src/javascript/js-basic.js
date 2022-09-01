02ee5df80637d60dd3e53d7b292e0e2b2a773c28
raja <raja.rakoto7@gmail.com>
2022-09-01 21:38:32 +0300

    src: init

---
+++ src/javascript/js-basic.js
@@ -0,0 +1,7 @@
+export const js_basic = {
+  "[test]: test": {
+		"prefix": "bbeo-js-test",
+		"body": ["console.log(${1:test})"],
+		"description": "[test]: test"
+	},
+}
---
+++ src/python/py-basic.js
@@ -0,0 +1,7 @@
+export const py_basic = {
+  "[test]: test": {
+		"prefix": "bbeo-py-test",
+		"body": ["print(${1:test})"],
+		"description": "[test]: test"
+	},
+}
