e0ac9a2d4383c99384f9bb03d9c3d8d95edfeb95
raja <raja.rakoto7@gmail.com>
2022-09-02 14:56:49 +0300

    src: add js_console | js_import

--- src/javascript/js-basic.js
+++
@@ -1,7 +0,0 @@
-export const js_basic = {
-  "[console]: console log": {
-		"prefix": "bbeo-js-clg",
-		"body": ["console.log(${1:object})"],
-		"description": "[console]: console log"
-	},
-}
---
+++ src/javascript/js-import.js
@@ -0,0 +1,14 @@
+export const js_import = {
+	'[import]: Imports entire module statement in ES6 syntax': {
+		prefix: 'bbeo-js-imp',
+		body: "import ${1:moduleName} from '${2:module}';",
+		description: '[import]: Imports entire module statement in ES6 syntax',
+	},
+	'[import]: [import]: Imports only a portion of the module in ES6 syntax (destructuring assignment)':
+		{
+			prefix: 'bbeo-js-imd',
+			body: "import { ${1:moduleName} } from '${2:module}';",
+			description:
+				'[import]: Imports only a portion of the module in ES6 syntax (destructuring assignment)',
+		},
+};
