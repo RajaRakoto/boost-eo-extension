0f62d0e83ea562ff217f9b9ad454352fab5e2e5f
raja <raja.rakoto7@gmail.com>
2022-09-02 20:05:17 +0300

    src: hotfix

--- src/javascript/js-module.js
+++ src/javascript/js-module.js
@@ -1,14 +1,14 @@
 export const js_module = {
-	'[import]: Imports entire module statement in ES6 syntax': {
+	'[module]: Imports entire module statement in ES6 syntax': {
 		prefix: 'bbeo-js-module-imp',
 		body: ['import ${1:module_name} from ${2:module_source};'],
-		description: '[import]: Imports entire module statement in ES6 syntax',
+		description: '[module]: Imports entire module statement in ES6 syntax',
 	},
-	'[import]: [import]: Imports only a portion of the module in ES6 syntax (destructuring assignment)':
+	'[module]: Imports only a portion of the module in ES6 syntax (destructuring assignment)':
 		{
 			prefix: 'bbeo-js-module-imd',
 			body: ['import { ${1:module_name} } from ${2:module_source};'],
 			description:
-				'[import]: Imports only a portion of the module in ES6 syntax (destructuring assignment)',
+				'[module]: Imports only a portion of the module in ES6 syntax (destructuring assignment)',
 		},
 };
