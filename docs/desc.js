b7c562164c1df3d69cc56419e6bba88c6f1eca59
raja <raja.rakoto7@gmail.com>
2022-09-04 16:42:33 +0300

    hotfix

--- docs/desc.js
+++ docs/desc.js
@@ -2,6 +2,6 @@ export const desc = `
 
 ### 📌 Description
 
-**BOOST-EO** is a set of custom code snippets & pattern available for VScode IDE to help you be more productive by providing helper methods 🚀.
+**BOOST-EO** is a set of custom code \`snippets\` & \`pattern\` available for VScode IDE to help you be more productive by providing helper methods 🚀.
 
 `
