0b6deae5cb7d1efda5d375d642c3fc18b46ce5f2
raja <raja.rakoto7@gmail.com>
2022-09-04 16:27:16 +0300

    refactor generateTable | getTable | exportSnippets | checkObjectValue | getOutput | refactorSourceList

--- utils/_utils-engine_.js
+++ utils/_utils-engine_.js
@@ -84,3 +84,96 @@ export function generatePatternsStatsData(source, key, prefix) {
 		.map(e => '`' + e + '`').join` `;
 	return patternsResult == '' ? '<div align="center"> n/a </div>' : '<div align="center">' + patternsResult + '</div>';
 }
+
+// table generator
+export function generateTable(data, language) {
+	let languageSyntax = '';
+	if (language == 'javascript') {
+		languageSyntax = 'js';
+	} else if (language == 'python') {
+		languageSyntax = 'py';
+	} else if (language == 'shellscript') {
+		languageSyntax = 'sh';
+	}
+	let table = `
+<table>
+<th>prefix</th>
+<th>body</th>
+<th>description</th>
+`;
+	for (let i = 0; i < data[0].length; i++) {
+		table += `
+<tr>
+<td>
+<strong>${data[0][i]}</strong>
+</td>
+<td>
+
+\`\`\`${languageSyntax}
+${data[1][i]}
+\`\`\`
+</td>
+<td>
+
+>${data[2][i]}
+</td>
+</tr>
+`;
+	}
+	table += `
+</table>
+`;
+	return table;
+}
+
+// table getter
+export function getTable(distPath, language) {
+	let prefixList = [];
+	let bodyList = [];
+	let descriptionList = [];
+
+	traverse(JSON.parse(fs.readFileSync(distPath, 'utf8'))).forEach(function (e) {
+		if (this.key == 'prefix') {
+			prefixList.push(e);
+		} else if (this.key == 'body') {
+			bodyList.push(e);
+		} else if (this.key == 'description') {
+			descriptionList.push(e);
+		}
+	});
+
+	return generateTable(
+		[prefixList, bodyList.map(body => `${body.join('\n')}`), descriptionList],
+		language,
+	);
+}
+
+// snippets exportation
+export function exportSnippets(distPath, sourceList) {
+	fs.writeFileSync(distPath, JSON.stringify(sourceList), 'utf8', err => {
+		console.log(err ? err : 'The file was saved!');
+	});
+}
+
+// object value checker
+export function checkObjectValue(object) {
+	return Object.keys(object).length === 0;
+}
+
+// output getter
+export function getOutput(sourceList, distPath, snippetsTitle, language) {
+	if (!checkObjectValue(sourceList)) {
+		exportSnippets(distPath, sourceList);
+		console.log(snippetsTitle);
+		console.log(getTable(distPath, language));
+	}
+}
+
+// source list refactoring
+export function refactorSourceList(sourceList) {
+	let objRefactored = {};
+	sourceList.forEach(source => {
+		objRefactored = { ...objRefactored, ...source };
+	});
+	return objRefactored;
+}
--- utils/boost-eo.js
+++ utils/boost-eo.js
@@ -1,5 +1,5 @@
-import traverse from 'traverse';
-import fs from 'fs';
+// engine importation
+import { getOutput, refactorSourceList } from './_utils-engine_.js';
 // docs importation
 import { banner } from '../docs/banner.js';
 import { contrib } from '../docs/contrib.js';
@@ -34,99 +34,6 @@ import { js_basic } from '../src/snippets/javascript/js-basic.js';
 import { js_date } from '../src/snippets/javascript/js-date.js';
 import { js_dom } from '../src/snippets/javascript/js-dom.js';
 import { js_bom } from '../src/snippets/javascript/js-bom.js';
-
-// table generator
-function generateTable(data, language) {
-	let languageSyntax = '';
-	if (language == 'javascript') {
-		languageSyntax = 'js';
-	} else if (language == 'python') {
-		languageSyntax = 'py';
-	} else if (language == 'shellscript') {
-		languageSyntax = 'sh';
-	}
-	let table = `
-<table>
-<th>prefix</th>
-<th>body</th>
-<th>description</th>
-`;
-	for (let i = 0; i < data[0].length; i++) {
-		table += `
-<tr>
-<td>
-<strong>${data[0][i]}</strong>
-</td>
-<td>
-
-\`\`\`${languageSyntax}
-${data[1][i]}
-\`\`\`
-</td>
-<td>
-
->${data[2][i]}
-</td>
-</tr>
-`;
-	}
-	table += `
-</table>
-`;
-	return table;
-}
-
-// table getter
-function getTable(distPath, language) {
-	let prefixList = [];
-	let bodyList = [];
-	let descriptionList = [];
-
-	traverse(JSON.parse(fs.readFileSync(distPath, 'utf8'))).forEach(function (e) {
-		if (this.key == 'prefix') {
-			prefixList.push(e);
-		} else if (this.key == 'body') {
-			bodyList.push(e);
-		} else if (this.key == 'description') {
-			descriptionList.push(e);
-		}
-	});
-
-	return generateTable(
-		[prefixList, bodyList.map(body => `${body.join('\n')}`), descriptionList],
-		language,
-	);
-}
-
-// snippets exportation
-function exportSnippets(distPath, sourceList) {
-	fs.writeFileSync(distPath, JSON.stringify(sourceList), 'utf8', err => {
-		console.log(err ? err : 'The file was saved!');
-	});
-}
-
-// object value checker
-function checkObjectValue(object) {
-	return Object.keys(object).length === 0;
-}
-
-// output getter
-function getOutput(sourceList, distPath, snippetsTitle, language) {
-	if (!checkObjectValue(sourceList)) {
-		exportSnippets(distPath, sourceList);
-		console.log(snippetsTitle);
-		console.log(getTable(distPath, language));
-	}
-}
-
-// source list refactoring
-function refactorSourceList(sourceList) {
-	let objRefactored = {};
-	sourceList.forEach(source => {
-		objRefactored = { ...objRefactored, ...source };
-	});
-	return objRefactored;
-}
 
 // source list data
 const javascriptSourceList = refactorSourceList([
@@ -185,4 +92,4 @@ console.log(`
 
 #### Enjoy it 😉
 </div>
-`)
+`);
