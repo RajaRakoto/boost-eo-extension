cf58021ebe8fb19484966034b43011911cb94ef8
raja <raja.rakoto7@gmail.com>
2022-09-04 16:18:23 +0300

    hotfix

--- docs/contrib.js
+++ docs/contrib.js
@@ -5,15 +5,10 @@ export const contrib = `
 ### 📌 Contribution
   
 - If you want to contribute to this project, here is a tool that will help you to generate snippets -> https://snippet-generator.app/
-- With each new code snippet added, you need to update \`all snippets\` & \`patterns\` file and regenerate the documentation, all in one command:
+- With each new code snippet added, you need to update all \`snippets\` & \`patterns\` file and regenerate the documentation, all in one command (in root directory):
 \`\`\`bash
-node boost-eo-engine.js > README.md
+cd utils && node boost-eo.js > ../README.md && cd ..
 \`\`\`
 - Then you can make a PR.
-
-<div align="center">
-
-#### Enjoy it 😉
-</div>
 
 `;
