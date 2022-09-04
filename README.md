7693dd8c4c8313b7be6589d687f35eb9b99d6332
raja <raja.rakoto7@gmail.com>
2022-09-04 21:01:15 +0300

    add demo

--- README.md
+++ README.md
@@ -22,6 +22,10 @@
 
 ### 📌 Description
 
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/boost-eo/boost-eo-demo.gif?raw=true" width="500">
+</div>
+
 **BOOST-EO** is a set of custom code `snippets` & `pattern` available for VScode IDE to help you be more productive by providing helper methods 🚀.
 
 
--- docs/desc.js
+++ docs/desc.js
@@ -2,6 +2,10 @@ export const desc = `
 
 ### 📌 Description
 
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/boost-eo/boost-eo-demo.gif?raw=true" width="500">
+</div>
+
 **BOOST-EO** is a set of custom code \`snippets\` & \`pattern\` available for VScode IDE to help you be more productive by providing helper methods 🚀.
 
 `
