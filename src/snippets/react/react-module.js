4b4017b778b19029b44c6c5f9842493118eabafd
raja <raja.rakoto7@gmail.com>
2022-09-12 17:27:57 +0300

    src: init react module snippets

--- src/snippets/react/react-module.js
+++ src/snippets/react/react-module.js
@@ -1,3 +1,7 @@
 export const react_module = {
-
-}
+	'[module]: Import react': {
+		prefix: 'bbeo-react-module-react',
+		body: ["import React from 'react';"],
+		description: '[module]: Import react',
+	},
+};
