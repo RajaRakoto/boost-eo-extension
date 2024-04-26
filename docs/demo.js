44827cd093859a5105ad3c201e57139b48159b16
raja <raja.rakoto7@gmail.com>
2024-04-26 21:13:31 +0300

    remove

--- docs/demo.js
+++
@@ -1,7 +0,0 @@
-export const demo = `
-
-<div align="center">
-<img src="https://github.com/RajaRakoto/github-docs/blob/master/boost-eo/boost-eo-demo.gif?raw=true" width="500">
-</div>
-
-`;
--- docs/install.js
+++
@@ -1,9 +0,0 @@
-export const install = `
-
----
-
-### 📌 Installation
-
-You can install it by typing \`boost-eo\` in the extension tab of your IDE
-
-`;
