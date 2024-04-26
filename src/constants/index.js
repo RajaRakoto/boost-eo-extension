16394baa93d03fcc562f6c3c359d1ed59dd9f5e3
raja <raja.rakoto7@gmail.com>
2024-04-26 21:20:27 +0300

    src: refact

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,17 +5,19 @@ export const emoji = {
 };
 
 // banner
-export const logo = `
+export const banner = {
+	logo: `
 <div align="center">
 <img src="https://github.com/RajaRakoto/github-docs/blob/master/plum/plum-gray.png?raw=true" width=256>
 </div>
-`;
-export const title = `
+  `,
+	title: `
 # boost-eo ${emoji.yellowHeart}
-`;
-export const badge = `
+  `,
+	badge: `
 ![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)
-`;
-export const description = `
+  `,
+	description: `
 Explore a curated compilation of helpful code snippets and clever design techniques designed to improve JavaScript developer productivity, easily accessible right within the VSCode environment.
-`;
+  `,
+};
