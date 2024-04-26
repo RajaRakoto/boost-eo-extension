c89845bc08a5f07358093e4033d0ae2f19894ff0
raja <raja.rakoto7@gmail.com>
2024-04-26 21:14:12 +0300

    src: init emoji + banner -> constants

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+// emoji
+export const emoji = {
+	yellowHeart: "💛",
+	pinned: "📌",
+};
+
+// banner
+export const logo = `
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/plum/plum-gray.png?raw=true" width=256>
+</div>
+`;
+export const title = `
+# boost-eo ${emoji.yellowHeart}
+`;
+export const badge = `
+![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)
+`;
+export const description = `
+Explore a curated compilation of helpful code snippets and clever design techniques designed to improve JavaScript developer productivity, easily accessible right within the VSCode environment.
+`;
