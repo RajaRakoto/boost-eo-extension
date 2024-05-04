5f8c152a1314372d51ef63e9024bb442fccfe26b
raja <raja.rakoto7@gmail.com>
2024-05-04 08:28:58 +0300

    src: pre valid constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,23 +1,42 @@
+// prod
+export const distFile = "README.md";
+
+// path
+export const dataPath = "dist/boost-eo-extension.code-snippets";
+export const sourcePath = "src/snippets";
+
 // emoji
 export const emoji = {
+	title: "📌",
+	category: "⚫",
 	yellowHeart: "💛",
-	pinned: "📌",
+	warning: "⚠️",
+	done: "✅",
+	failed: "❌",
 };
+
+// table
+export const tableHeader = `
+| Prefix | Description |`;
 
 // banner
 export const banner = {
-	logo: `
-<div align="center">
-<img src="https://github.com/RajaRakoto/github-docs/blob/master/plum/plum-gray.png?raw=true" width=256>
-</div>
-  `,
-	title: `
-# boost-eo ${emoji.yellowHeart}
-  `,
+	logo: `<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/boost-eo-extension/boost-eo.png?raw=true" width=256>
+</div>`,
+	title: `\n# boost-eo-extension ${emoji.yellowHeart}`,
 	badge: `
-![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)
-  `,
+![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)`,
 	description: `
-Explore a curated compilation of helpful code snippets and clever design techniques designed to improve JavaScript developer productivity, easily accessible right within the VSCode environment.
+Explore a curated compilation of helpful code snippets and clever design techniques designed to improve JavaScript developer productivity, easily accessible right within the VSCode environment.`,
+	usage: `
+**Usage**: You can install it by typing "boost-eo" in the extension tab of your IDE
   `,
 };
+
+// others
+export const backToTop = `
+<div align="left">
+    <b><a href="#-table-of-contents">↥ Back To Top</a></b>
+</div>
+`;
